acdb25d96780eefec8bbf73fe743067c1699849f
Redouane <damcorh@gmail.com>
2022-03-23 01:18:48 +0100

    add a feature with 'Dafalgan'

--- pharmacy.js
+++ pharmacy.js
@@ -19,6 +19,11 @@ export class Pharmacy {
         if (this.drugs[i].benefit > 0) {
           if (this.drugs[i].name != "Magic Pill") {
             this.drugs[i].benefit = this.drugs[i].benefit - 1;
+            if (this.drugs[i].name == "Dafalgan") {
+              if (this.drugs[i].benefit > 0) {
+                this.drugs[i].benefit = this.drugs[i].benefit - 1;
+              }
+            }
           }
         }
       } else {
@@ -47,11 +52,16 @@ export class Pharmacy {
             if (this.drugs[i].benefit > 0) {
               if (this.drugs[i].name != "Magic Pill") {
                 this.drugs[i].benefit = this.drugs[i].benefit - 1;
+                if (this.drugs[i].name == "Dafalgan") {
+                  if (this.drugs[i].benefit > 0) {
+                    this.drugs[i].benefit = this.drugs[i].benefit - 1;
+                  }
+                }
               }
             }
           } else {
             this.drugs[i].benefit =
-              this.drugs[i].benefit - this.drugs[i].benefit;
+              this.drugs[i].benefit + this.drugs[i].benefit;
           }
         } else {
           if (this.drugs[i].benefit < 50) {
